9cf4b2d4a18ade7048e8e6622982ada84533214e
daria <daria.maksimova2020@gmail.com>
2024-02-14 11:00:22 +0300

    small fixes

--- config_data/config.py
+++ config_data/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 if not find_dotenv():
-    exit("Environment variables are not loaded because the .env file is missing")
+    exit("Environment variables are not loaded because the env file is missing")
 else:
     load_dotenv()
 
--- utils/misc/set_commands.py
+++
@@ -1,8 +0,0 @@
-from aiogram import types
-from config_data.config import DEFAULT_COMMANDS
-
-
-async def set_commands(dp):
-    await dp.bot.set_my_commands(
-        [types.BotCommand(name, description) for name, description in DEFAULT_COMMANDS]
-    )
